a03642c8476173cce8de0b36b17a0e0c4c2bdc1c
Fernando Naufel <fnaufel@gmail.com>
2026-05-16 15:37:21 -0300

    Inspect pocket candidates LMDB

--- notebooks/target_fishing.py
+++ notebooks/target_fishing.py
@@ -13,12 +13,21 @@ def _():
 
 @app.cell
 def _():
-    import biosensia_target_fishing as bsia
+    import importlib
+    import lmdb
+    import pickle
     import polars as pl
     import altair as alt
 
     alt.data_transformers.enable("vegafusion")
-    return alt, bsia
+    return alt, importlib, lmdb, pickle
+
+
+@app.cell
+def _(importlib):
+    import biosensia_target_fishing as tf
+    importlib.reload(tf)
+    return (tf,)
 
 
 @app.cell(hide_code=True)
@@ -26,14 +35,16 @@ def _(mo):
     mo.md(r"""
     # Pocket candidates
 
+    ## Building the LMDB file of pockets
+
     We built an LMDB file from the `external/DrugCLIP/data/pdb/combine_set` directory, using function `build_candidate_pockets_frame` in file [`biosensia_target_fishing.py`](biosensia_target_fishing.py). Here is a summary:
     """)
     return
 
 
 @app.cell
-def _(bsia):
-    df = bsia.build_candidate_pockets_frame(lmdb_path='data/candidate_pockets.lmdb')
+def _(tf):
+    df = tf.build_candidate_pockets_frame(lmdb_path='data/candidate_pockets.lmdb')
     return (df,)
 
 
@@ -68,7 +79,87 @@ def _(alt, df):
 
 
 @app.cell
-def _():
+def _(mo):
+    mo.md(r"""
+    This is the set of candidate pockets that will be encoded and ranked according to the similarity to the query molecule(s).
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Inspecting `candidate_pockets.lmdb`
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Each record consists of
+
+    - A numeric ASCII string as the key.
+    - As the value, a pickled dictionary with keys:
+      - 'pocket': the PDB 4-charcater code of the complex.
+      - 'pocket_atoms': the list of atoms comprising the pocket.
+      - 'pocket_coordinates': the list of 3D coordinates of the atoms.
+    """)
+    return
+
+
+@app.cell
+def _(lmdb, pickle):
+    def read_lmdb(lmdb_path, head_n=None):
+
+        env = lmdb.open(
+            lmdb_path,
+            subdir=False,
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False,
+            max_readers=256,
+        )  
+
+        txn = env.begin()
+        # LMDB cursor order is lexicographic byte order, not numeric order,
+        # so we sort the keys:
+        keys = sorted(
+            txn.cursor().iternext(values=False), 
+            key=lambda k: int(k.decode('ascii'))
+        )
+    
+        out_dict = {}
+        i = 1
+
+        for idx in keys:
+            datapoint_pickled = txn.get(idx)
+            data = pickle.loads(datapoint_pickled)
+            out_dict[idx] = data
+            i += 1
+            if head_n is not None and i > head_n:
+                break
+            keys = txn.cursor()
+
+        env.close()
+        return out_dict
+
+
+    return (read_lmdb,)
+
+
+@app.cell
+def _(read_lmdb):
+    read_lmdb('data/candidate_pockets.lmdb', 5)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Building the query molecule(s) `lmdb` file
+    """)
     return
 
 
