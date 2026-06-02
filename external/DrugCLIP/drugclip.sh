b097cfd895817bb9d6c75a3735c11a03f1a1515a
Fernando Naufel <fnaufel@gmail.com>
2026-06-02 13:57:48 -0300

    Format DrugCLIP training command options

--- external/DrugCLIP/drugclip.sh
+++ external/DrugCLIP/drugclip.sh
@@ -60,19 +60,47 @@ fi
 
 export NCCL_ASYNC_ERROR_HANDLING=1
 export OMP_NUM_THREADS=1
-python -m torch.distributed.launch --nproc_per_node=$n_gpu --master_port=$MASTER_PORT $(which unicore-train) $data_path --user-dir ./unimol --train-subset train --valid-subset valid \
-       --num-workers 8 --ddp-backend=c10d \
-       --task drugclip --loss in_batch_softmax --arch drugclip  \
+python \
+       -m torch.distributed.launch \
+       --nproc_per_node="$n_gpu" \
+       --master_port="$MASTER_PORT" \
+       "$(which unicore-train)" \
+       "$data_path" \
+       --user-dir ./unimol \
+       --train-subset train \
+       --valid-subset valid \
+       --num-workers 8 \
+       --ddp-backend=c10d \
+       --task drugclip \
+       --loss in_batch_softmax \
+       --arch drugclip \
        --max-pocket-atoms 256 \
-       --optimizer adam --adam-betas "(0.9, 0.999)" --adam-eps 1e-8 --clip-norm 1.0 \
-       --lr-scheduler polynomial_decay --lr $lr --warmup-ratio $warmup --max-epoch $epoch --batch-size $batch_size --batch-size-valid $batch_size_valid \
-       --fp16 --fp16-init-scale 4 --fp16-scale-window 256 --update-freq $update_freq --seed 1 \
-       --tensorboard-logdir $tsb_dir \
-       --log-interval 100 --log-format simple \
+       --optimizer adam \
+       --adam-betas "(0.9, 0.999)" \
+       --adam-eps 1e-8 \
+       --clip-norm 1.0 \
+       --lr-scheduler polynomial_decay \
+       --lr "$lr" \
+       --warmup-ratio "$warmup" \
+       --max-epoch "$epoch" \
+       --batch-size "$batch_size" \
+       --batch-size-valid "$batch_size_valid" \
+       --fp16 \
+       --fp16-init-scale 4 \
+       --fp16-scale-window 256 \
+       --update-freq "$update_freq" \
+       --seed 1 \
+       --tensorboard-logdir "$tsb_dir" \
+       --log-interval 100 \
+       --log-format simple \
        --validate-interval 1 \
-       --best-checkpoint-metric valid_bedroc --patience 2000 --all-gather-list-size 2048000 \
-       --save-dir $save_dir --tmp-save-dir $tmp_save_dir --keep-last-epochs 5 \
+       --best-checkpoint-metric valid_bedroc \
+       --patience 2000 \
+       --all-gather-list-size 2048000 \
+       --save-dir "$save_dir" \
+       --tmp-save-dir "$tmp_save_dir" \
+       --keep-last-epochs 5 \
        --find-unused-parameters \
        --maximize-best-checkpoint-metric \
-       --finetune-pocket-model $finetune_pocket_model \
-       --finetune-mol-model $finetune_mol_model \
+       --finetune-pocket-model "$finetune_pocket_model" \
+       --finetune-mol-model "$finetune_mol_model"
