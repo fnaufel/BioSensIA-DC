7ac9974f78f19669ae4c291fc7615713715b3c2e
Fernando Naufel <fnaufel@gmail.com>
2026-06-06 22:39:32 -0300

    Build json with all pocket-ligand pairs

--- scratch/build_pairs_frame.py
+++ scratch/build_pairs_frame.py
@@ -1,4 +1,4 @@
 import analyze_pdb_complexes as apc
 
-df = apc.build_complexes_frame(limit=100)
-apc.write_frame(df, './scratch/2026-06-06-pairs.json')
+df = apc.build_complexes_frame()
+apc.write_frame(df, '2026-06-06-pairs.json')
